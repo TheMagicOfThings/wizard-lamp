0948f66b2a59731f0d11c34d12b3a9a2810b73d5
lyudmilatanasov <45456962+lyudmilatanasov@users.noreply.github.com>
2020-04-09 17:11:58 +0100

    Add animation

--- DIY_Wizard_Lamp_Rainbow/DIY_Wizard_Lamp_Rainbow.ino
+++ DIY_Wizard_Lamp_Rainbow/DIY_Wizard_Lamp_Rainbow.ino
@@ -25,10 +25,6 @@ int previousSate = LOW;       // the previousSate reading from the input pin
 long time = 0;         // the last time the output pin was toggled
 long debounce = 100;   // delay between readings
 
-//Set the pixel colour
-// strip.Color takes RGB values, from 0,0,0 up to 255,255,255
-int r = 128, g = 255, b = 255;
-
 void setup()
 {
   //Initialise the sensor pin as an input with a pullup resistor
@@ -41,12 +37,12 @@ void setup()
 //Main program loop
 void loop(){
 
-//Turn the light ON 
+//Turn the light ON
 if (currentState == HIGH) {
-  singleColor();
+  cycleColors();
 }
 
-//Turn the light OFF
+//Turn light OFF
   if (currentState == LOW) {
     turnOff();
   }
@@ -55,14 +51,43 @@ if (currentState == HIGH) {
   checkState(); 
 }
 
-//Single colour animation
-void singleColor(){
-  for(int i=0; i<numberOfPixels; i++) {
-    strip.setPixelColor(i,r,g,b);
-  }
-  strip.show(); 
-  }
 
+void cycleColors(){
+  int i, j;
+  for(i=0; i<256; i++) {
+
+    for(j=0; j<strip.numPixels(); j++) {
+      strip.setPixelColor(j, colorWheel((j+i) & 255));
+    }
+
+    strip.show();
+
+    //Delay between colour changes in ms
+    delay(200);
+
+    //Keep checking for a wand tap during the animation
+    checkState(); 
+    if (currentState == LOW) {
+    turnOff();
+    break;
+    }
+  }
+}
+
+
+// Transition the colours
+unsigned long colorWheel(byte colorPosition) {
+  colorPosition = 255 - colorPosition;
+  if(colorPosition < 85) {
+    return strip.Color(255 - colorPosition * 3, 0, colorPosition * 3);
+  }
+  if(colorPosition < 170) {
+    colorPosition -= 85;
+    return strip.Color(0, colorPosition * 3, 255 - colorPosition * 3);
+  }
+  colorPosition -= 170;
+  return strip.Color(colorPosition * 3, 255 - colorPosition * 3, 0);
+}
 
 //Function to turnoff the led strip
 void turnOff(){
